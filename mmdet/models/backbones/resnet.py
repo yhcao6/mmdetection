5b54f0ae9e1b4572b3c5cf529d085fc843e10bec
beansi <wayne.zw@outlook.com>
2019-10-25 17:26:04 +0800

    add detectron2 setting

--- mmdet/models/backbones/resnet.py
+++ mmdet/models/backbones/resnet.py
@@ -496,6 +496,7 @@ class ResNet(nn.Module):
     def init_weights(self, pretrained=None):
         if isinstance(pretrained, str):
             logger = logging.getLogger()
+            logger.info(print(self))
             load_checkpoint(self, pretrained, strict=False, logger=logger)
         elif pretrained is None:
             for m in self.modules():
