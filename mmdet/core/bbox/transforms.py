c5aebf816c4b7f56a774c8d59acabb85c4f69d63
zhangwenwei <wayne.zw@outlook.com>
2019-12-13 11:04:37 +0800

    fix numclasses -1 bug

--- mmdet/core/bbox/transforms.py
+++ mmdet/core/bbox/transforms.py
@@ -197,12 +197,12 @@ def bbox2result(bboxes, labels, num_classes):
     """
     if bboxes.shape[0] == 0:
         return [
-            np.zeros((0, 5), dtype=np.float32) for i in range(num_classes - 1)
+            np.zeros((0, 5), dtype=np.float32) for i in range(num_classes)
         ]
     else:
         bboxes = bboxes.cpu().numpy()
         labels = labels.cpu().numpy()
-        return [bboxes[labels == i, :] for i in range(num_classes - 1)]
+        return [bboxes[labels == i, :] for i in range(num_classes)]
 
 
 def distance2bbox(points, distance, max_shape=None):
