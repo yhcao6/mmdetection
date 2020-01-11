2681ce9b0bf66abf3294e5456664bf26bec861e2
wwzhang <wayne.zw@outlook.com>
2020-01-11 18:56:13 +0800

    fix mask config bug

--- configs/detectron2/mask_rcnn_r50_fpn_ms1333_detectron2-caffe-freezeBN_l1-loss_roialign-v2_1x.py
+++ configs/detectron2/mask_rcnn_r50_fpn_ms1333_detectron2-caffe-freezeBN_l1-loss_roialign-v2_1x.py
@@ -101,7 +101,7 @@ train_cfg = dict(
             num=512,
             pos_fraction=0.25,
             neg_pos_ub=-1,
-            add_gt_as_proposals=False),
+            add_gt_as_proposals=True),
         mask_size=28,
         pos_weight=-1,
         debug=False))
@@ -129,13 +129,12 @@ img_norm_cfg = dict( # The
     mean=[103.530, 116.280, 123.675], std=[1.0, 1.0, 1.0], to_rgb=False)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
-    dict(type='LoadAnnotations', with_bbox=True, with_mask=True, poly2mask=False),
+    dict(type='LoadAnnotations', with_bbox=True, with_mask=True),
     dict(type='Resize', 
-         img_scale=[(1333, 672),],
-#          img_scale=[(1333, 640),(1333, 672), (1333, 704), 
-#                     (1333, 736), (1333, 768), (1333, 800)],
+         img_scale=[(1333, 640),(1333, 672), (1333, 704), 
+                    (1333, 736), (1333, 768), (1333, 800)],
          multiscale_mode="value", keep_ratio=True),
-    dict(type='RandomFlip', flip_ratio=0),
+    dict(type='RandomFlip', flip_ratio=0.5),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size_divisor=32),
     dict(type='DefaultFormatBundle'),
